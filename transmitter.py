7d69b10e75cf879e92063a000fbad8985920b874
matpech <47293035+mtroll21@users.noreply.github.com>
2022-01-14 12:30:33 +0100

    Update transmitter.py

--- transmitter.py
+++ transmitter.py
@@ -1,36 +1,34 @@
-in terminal:
-sudo apt-get install rpi.gpio
 
-TRANSMISSION
+#TRANSMISSION
 	
-	//imports
+	#imports
 	inport RPi.GPIO as io
 	import time
-	import letters.py //import letter map
+	import letters.py #import letter map
 	
 	io.setmode(io.BCM)
 	
-	//define
+	#define
 	laser = 27
 	io.setup(laser, io.OUT)
 	
 	
-	input = raw_input("") //usr input
-	letters[] = input.split("") //split text to letters
+	input = raw_input("") #usr input
+	letters[] = input.split("") #split text to letters
 	loopIf = letter.lenght
-	while(loopIf >= 1): //loop
-		io.output(laser, True //convert letters to binaray and send data
+	while(loopIf >= 1): #loop
+		io.output(laser, True #convert letters to binaray and send data
 		time.sleep(<time delay>)
 
 
 
 
-RECEPTION //idk
+#RECEPTION (idk)
 
-mesure laser on fotoresistor
-convert patern to letters
-conect letters
-output to terminal
+#mesure laser on fotoresistor
+#convert patern to letters
+#conect letters
+#output to terminal
 
 
 
