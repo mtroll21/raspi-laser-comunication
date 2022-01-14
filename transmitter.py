9c97a0df232c8cd4d8266e5f2b362beee20ac845
matpech <47293035+mtroll21@users.noreply.github.com>
2022-01-14 13:08:57 +0100

    Update transmitter.py

--- transmitter.py
+++ transmitter.py
@@ -1,21 +1,22 @@
-
 #TRANSMISSION
+#by mtroll21
+#use this o the transmitter side (the one with the laser)
 	
-	#imports
-	inport RPi.GPIO as io
-	import time
-	import letters.py #import letter map
-	
-	io.setmode(io.BCM)
-	
-	#define
-	laser = 27
-	io.setup(laser, io.OUT)
-	
-	
-	input = raw_input("") #usr input
-	letters[] = input.split("") #split text to letters
-	loopIf = letter.lenght
-	while(loopIf >= 1): #loop
-		io.output(laser, True #convert letters to binaray and send data
-		time.sleep(<time delay>)
+#imports
+inport RPi.GPIO as io
+import time
+import letters.py #import letter map
+
+io.setmode(io.BCM)
+
+#define
+laser = 27
+io.setup(laser, io.OUT)
+
+
+input = raw_input("") #usr input
+letters[] = input.split("") #split text to letters
+loopIf = letter.lenght
+while(loopIf >= 1): #loop
+	io.output(laser, True #convert letters to binaray and send data
+	time.sleep(<time delay>)
